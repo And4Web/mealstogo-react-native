170c7746778b5707e9b8386c177203efe03b9bbd
And4Web <anands2807@gmail.com>
2022-01-19 09:06:38 -0800

    some changes

--- App.js
+++ App.js
@@ -5,13 +5,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-     
+     <Text>Hello World</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-   
+   flex: 1,
+   alignItems: 'center',
+   justifyContent: 'center',
   },
 });
